6b77d73c4db3697ecf3fa5bcd4bbc0422461ecd1
CL0990499 <cl0990499@otc.edu>
2025-03-05 11:44:17 -0600

    Add MyFirstProgram.cpp

---
+++ MyFirstProgram.cpp/MyFirstProgram.cpp.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>  
+using namespace std;
+
+int main() {
+    
+    int myInt = 42;
+    float myFloat = 3.14;
+    double myDouble = 2.71828;
+    char myChar = 'C';
+    bool myBool = true;
+    string myString = "Hello, World!";
+
+    cout << "Integer: " << myInt << endl;
+    cout << "Float: " << myFloat << endl;
+    cout << "Double: " << myDouble << endl;
+    cout << "Character: " << myChar << endl;
+    cout << "Boolean: " << myBool << endl;
+    cout << "String: " << myString << endl;
+
+    return 0;
+}
